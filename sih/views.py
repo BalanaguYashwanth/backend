0434ee9873dce41fff138e34e4bab8ec8a6f0fd9
BalanaguYashwanth <balanaguyashwanth@gmail.com>
2020-11-30 11:49:54 +0530

    second commit

--- sih/views.py
+++ sih/views.py
@@ -7,12 +7,12 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated,AllowAny,IsAuthenticatedOrReadOnly
+from rest_framework.decorators import action
 
 class datasViewset(viewsets.ModelViewSet):
-    permission_classes = (IsAuthenticated,)
-
     serializer_class=datasSerializer
     queryset=datas.objects.all()
+
 
 
 class register(APIView):
